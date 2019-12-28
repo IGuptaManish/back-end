f5c297ce5a7b74091915c634c613ace30402450d
IGuptaManish <59290210+IGuptaManish@users.noreply.github.com>
2019-12-28 15:49:42 +0530

    Order Service initial commit

---
+++ order-service/src/main/java/com/practice/orderservice/OrderServiceStarter.java
@@ -0,0 +1,13 @@
+package com.practice.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceStarter.class, args)
+	}
+
+}
